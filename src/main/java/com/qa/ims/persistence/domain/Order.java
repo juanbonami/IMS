89f2ae19b4cd1db257acdfbdd6d5dfba384cc871
juan <juanbonami@gmail.com>
2021-06-01 11:22:26 -0500

    starting ordres template in persistence doimain

---
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -0,0 +1,32 @@
+package com.qa.ims.persistence.domain;
+
+public class Order {
+	
+	private long orderId;
+	private long customerId;
+	
+	public Order(long orderId, long customerId) {
+		this.setOrderId(orderId);
+		this.setCustomerId(customerId);
+	}
+
+	public long getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(long orderId) {
+		this.orderId = orderId;
+	}
+
+	public long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+	
+	
+	
+
+}
