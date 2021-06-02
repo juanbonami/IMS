a9caa1fd3f2f078f8a0621186072eec138348a9a
juan <juanbonami@gmail.com>
2021-06-02 01:29:54 -0500

    created Item template in the domain package

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -1,5 +1,41 @@
 package com.qa.ims.persistence.domain;
 
 public class Item {
-
+	
+	private long itemId;
+	private String itemName;
+	private int value;
+	
+	public Item(long itemId, String itemName, int value) {
+		this.setItemId(itemId);
+		this.setItemName(itemName);
+		this.setValue(value);
+	}
+	
+	public long getItemId() {
+		return itemId;
+	}
+	
+	public void setItemId(long itemId) {
+		this.itemId = itemId;
+	}
+	
+	public String getItemName() {
+		return itemName;
+	}
+	
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	
+	
 }
