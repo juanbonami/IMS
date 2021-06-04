0e0084b6d5b6471235558fb82389e9b0a8b3ad54
juan <juanbonami@gmail.com>
2021-06-03 23:17:22 -0500

    JB: created controller for junction table

---
+++ src/main/java/com/qa/ims/controller/OrderProductController.java
@@ -0,0 +1,63 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.OrderProductDAO;
+import com.qa.ims.persistence.domain.OrderProducts;
+import com.qa.ims.utils.Utils;
+
+public class OrderProductController implements CrudController<OrderProducts> {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	private OrderProductDAO orderProductDao;
+	private Utils utils;
+
+	public OrderProductController(OrderProductDAO orderProductDao, Utils utils) {
+		super();
+		this.orderProductDao = orderProductDao;
+		this.utils = utils;
+	}
+
+	@Override
+	public List<OrderProducts> readAll() {
+		List<OrderProducts> orderProducts = orderProductDao.readAll();
+		for (OrderProducts product : orderProducts) {
+			LOGGER.info(product);
+		}
+		return orderProducts;
+	}
+
+	@Override
+	public OrderProducts create() {
+		LOGGER.info("Please enter order id");
+		long orderId = utils.getLong();
+		LOGGER.info("Please enter the item id");
+		long itemId = utils.getLong();
+		OrderProducts orderProducts = orderProductDao.create(new OrderProducts(orderId, itemId));
+		LOGGER.info("Customer created");
+		return orderProducts;
+	}
+
+	@Override
+	public OrderProducts update() {
+		LOGGER.info("Please enter the id of the order you would like to update");
+		Long orderId = utils.getLong();
+		LOGGER.info("Please enter new item id");
+		long itemId = utils.getLong();
+		OrderProducts orderProducts = orderProductDao.update(new OrderProducts(orderId, itemId));
+		LOGGER.info("Order Updated");
+		return orderProducts;
+	}
+
+	@Override
+	public int delete() {
+		LOGGER.info("Please enter the id of the order you would like to delete");
+		Long orderId = utils.getLong();
+		return orderProductDao.delete(orderId);
+	}
+
+}
